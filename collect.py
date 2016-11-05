e0a73a4df59a41c7ee5228d294e11ae418066eb1
jkao1 <jkao1@stuy.edu>
2016-11-05 00:44:08 -0400

    3

--- collect.py
+++ collect.py
@@ -3,23 +3,40 @@ import re
 
 def main():
     main_url = "http://www.themusicallyrics.com/h/351-hamilton-the-musical.html"
-    f = urllib.urlopen(main_url)
-    s = f.read()
+    f = urllib.urlopen(main_url).read()
+    
+    hrefs = re.findall(r'/h/.+lyrics.*\.html',f)
 
-    table_s = s.find('<form action="http://www.themusicallyrics.com/h/351-hamilton-the-musical.html" method="post" name="adminForm">')
-    table_e = s.find('<input type="hidden" name="id" value="351">', table_s)
-    table_html = s[table_s:table_e].split('</tr>')[3:]
+    for href in hrefs[4:5]:
+        titleRegex = re.compile(r'\d{4}-.*[^\.html]') 
+        title = titleRegex.search(href).group()
+        
+        f = urllib.urlopen(main_url+href).read()
+        
+        f = re.sub(r'<[^>]*>','\n', f) # deleted tags
+        f = re.sub(r'(\sand\s)?\([^\)]*\)','', f) # deleted parantheses
+        f = re.sub(u'\u2019'.encode('utf8'),'', f) # deleted curved apostrophes
+        
+        startRegex = re.compile(r'[A-Z][A-Z]+:')
+        try:
+            start = startRegex.search(f).group()
 
-    links = []
-    for entry in table_html[:2]:
-        href_s = entry.find('<a href="') + len('<a href="')
-        href_e = entry.find('">', href_s)
-        href = entry[href_s:href_e]
-        
-        s = urllib.urlopen(main_url + href).read()
-        mark = '</em><br /><br />'
-        piece_s = s.find(mark) + len(mark)
-        piece_e = s.find('</p>',piece_s)
-        
-        
+        except AttributeError:
+            if 'King George' in f:
+                startRegex = re.compile(r'\w+\ssay')
+                start = startRegex.search(f).group()
+            else:
 
+        f_s = f.find(start)
+        f_e = f.find('lyrics',f_s)
+        f = f[f_s:f_e]
+        
+        """
+        lyricFile = open('data/'+title+'.txt', 'w')
+        lyricFile.write(f)
+        lyricFile.close()
+        print "wrote "+title+".txt"
+"""
+        
+if __name__ == '__main__':
+    main()
