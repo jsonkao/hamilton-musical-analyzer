aaf27ce0ed000a132edc05e5c4bc4e3d3a3ebd86
jkao1 <jkao1@stuy.edu>
2016-11-05 14:35:31 -0400

    so trigereed

--- collect.py
+++ collect.py
@@ -1,42 +1,54 @@
 import urllib
 import re
+omg = '3704-my-shot-lyrics-hamilton.html'
+def writeFile(href):
+    main_url = "http://www.themusicallyrics.com/h/351-hamilton-the-musical-lyrics/"
 
-def main():
+    titleRegex = re.compile(r'\d{4}-.*[^\.html]')
+    title = titleRegex.search(href).group()
+
+    f = urllib.urlopen(main_url + href).read()
+
+    f = re.sub(r'<[^>]*>','\n',f) # tags
+    f = re.sub(r'(\sand\s)?\([^\)]*\)','',f) # parantheses
+    f = re.sub(u'\u2019'.encode('utf-8'),'',f)
+    
+    startRegex = re.compile(r'[A-Z][A-Z]+|[A-Z][a-z]+\ssay|I\ssaved|\[[A-Za-z\/:\s,]*\]')
+    start = startRegex.search(f).group()
+    f = f[f.find(start):f.rfind('Read more:')]
+
+    return [title, f]
+
+def gatherHrefs():
     main_url = "http://www.themusicallyrics.com/h/351-hamilton-the-musical.html"
     f = urllib.urlopen(main_url).read()
-    
-    hrefs = re.findall(r'/h/.+lyrics.*\.html',f)
+    hrefs = re.findall(r'\d{4}[a-z\-]+\.html',f)
+    return hrefs
 
-    for href in hrefs[4:5]:
-        titleRegex = re.compile(r'\d{4}-.*[^\.html]') 
-        title = titleRegex.search(href).group()
+def main():
+    hrefs = gatherHrefs()
+    for href in hrefs:
         
-        f = urllib.urlopen(main_url+href).read()
-        
-        f = re.sub(r'<[^>]*>','\n', f) # deleted tags
-        f = re.sub(r'(\sand\s)?\([^\)]*\)','', f) # deleted parantheses
-        f = re.sub(u'\u2019'.encode('utf8'),'', f) # deleted curved apostrophes
-        
-        startRegex = re.compile(r'[A-Z][A-Z]+:')
-        try:
-            start = startRegex.search(f).group()
+        fileContent = write(href)
+        f = open('data/'+fileContent[0]+'.txt','w')
+        f.write(fileContent[1])
+        f.close()
 
-        except AttributeError:
-            if 'King George' in f:
-                startRegex = re.compile(r'\w+\ssay')
-                start = startRegex.search(f).group()
-            else:
+        
+        #3665-electionof1800, 3669-burn, 3677-cabinetbattle, 3682-catbinet battle
+faulty=['3665-election-of-1800-lyrics-hamilton.html',
+        '3677-cabinet-battle-2-lyrics-hamilton.html',
+        '3682-cabinet-battle-1-lyrics.html',
+        '3669-burn-lyrics-hamilton-the-musica',
+        '3702-the-schuyler-sisters-lyrics',
+        '3704-my-shot-lyrics-hamilton.html'
+        
+        ]
 
-        f_s = f.find(start)
-        f_e = f.find('lyrics',f_s)
-        f = f[f_s:f_e]
-        
-        """
-        lyricFile = open('data/'+title+'.txt', 'w')
-        lyricFile.write(f)
-        lyricFile.close()
-        print "wrote "+title+".txt"
-"""
-        
-if __name__ == '__main__':
-    main()
+
+def makeup(n):
+    fileContent = writeFile(faulty[n])
+    f = open('data/'+fileContent[0]+'.txt','w')
+    f.write(fileContent[1])
+    d=open('data/'+fileContent[0]+'.txt','rU').read()[:50]
+    f.close()
