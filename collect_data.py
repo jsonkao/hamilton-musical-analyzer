627f2d799f457bbf2f463b28bda07ae5cae24c02
jkao1 <jkao1@stuy.edu>
2016-11-25 01:10:45 -0500

    sleep. next problem: javascript-embedded spotify text

--- collect_data.py
+++ collect_data.py
@@ -1,11 +1,24 @@
 import urllib
 import re
 
-def loadLinks():
-    HTML = urllib.urlopen('http://atlanticrecords.com/HamiltonMusic/').read()
-    HTML = HTML[HTML.rfind('<ul class="lyrics'):HTML.rfind('<div class="bottom')]
+def h():
+
+    # loads HTML
+    root_url = 'http://atlanticrecords.com/HamiltonMusic/'
+    HTML = urllib.urlopen(root_url).read()
+    HTML = HTML[HTML.find('<ul class="lyrics'):HTML.find('<div class="bottom')]
+    
     # returns a list of (href,title)
     linkRegex = re.compile(r'<a href="([^"]*)"\sdata-ref="[^"]*">([^<]*)')
-    L = linkRegex.findall(HTML)
-    return L
-    
+    links = linkRegex.findall(HTML)
+    return links
+    for i in range(0,len(links)):
+        href = links[i][0]
+        title = links[i][1].replace(' ','_')
+
+        f = urllib.urlopen(root_url + href).read()
+        f = f[f.find('<div class="rg_embed_body'):f.find('<div class="rg_embed_footer')]
+        print len(f)
+        f = re.sub(r'<[^>]*>','',f)
+        return f
+        
