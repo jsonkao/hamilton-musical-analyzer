a47396ffee6603bc3e6419b3fd9cc05f22a2d0d1
jkao1 <jkao1@stuy.edu>
2016-11-25 00:55:14 -0500

    finished loadLinks()

--- collect_data.py
+++ collect_data.py
@@ -3,9 +3,9 @@ import re
 
 def loadLinks():
     HTML = urllib.urlopen('http://atlanticrecords.com/HamiltonMusic/').read()
-    
+    HTML = HTML[HTML.rfind('<ul class="lyrics'):HTML.rfind('<div class="bottom')]
     # returns a list of (href,title)
     linkRegex = re.compile(r'<a href="([^"]*)"\sdata-ref="[^"]*">([^<]*)')
     L = linkRegex.findall(HTML)
-
+    return L
     
