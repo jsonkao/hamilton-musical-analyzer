f1a6946f859f259b2a2a16eaa4e902446adc7d3a
jkao1 <jkao1@stuy.edu>
2016-11-26 12:29:57 -0500

    created list_formats()

--- parse_to_json.py
+++ parse_to_json.py
@@ -1,8 +1,19 @@
 import re
 from collections import defaultdict
 
-def find_actors(filename):
-    
+
+def list_formats():
+    for filename in os.listdir(os.getcwd() + '/data'):
+	with open(filename) as f:
+            s = f.read()
+	reg = re.compile(r'\[[^\]]*\]')
+	a = reg.findall(s)
+	for thing in a:
+		d.add(thing)
+	print 'finished ' + i
+
+def get_actors(filename):
+    return
     
 def parse(filename):
     with open(filename) as f:
