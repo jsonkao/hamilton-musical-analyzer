79846f0459e68fa99d49d704fa6c67ba205363f0
jkao1 <jkao1@stuy.edu>
2016-11-28 14:45:40 -0500

    working on get_authors

--- parse_to_json.py
+++ parse_to_json.py
@@ -1,20 +1,39 @@
-import re
 from collections import defaultdict
+import re
+import os
 
+def get_actors(L):
+    L = L.split(' ')
+    actors = []
+
+    men = ['hamilton','burr','lafayette','jefferson','washington','laurens','mulligan','madison']
+    women = ['eliza','angelica','peggy']
+
+    if 'all' in L:
+        return
+        
+    while w < len(L):
+        if 'all' == L[w]:
+            new_ = eval(L[w+1])
+            if 'except' in L:
+                actors += eval(L[w+1]).remove(L.index('excerpt')+1)
+                L[L.index('except')+1]
+        else:
+            actors.append(L[w])
 
 def list_formats():
-    for filename in os.listdir(os.getcwd() + '/data'):
-	with open(filename) as f:
+    d = set()
+    os.chdir('data/')
+    for filename in os.listdir(os.getcwd()):
+        with open(filename) as f:
             s = f.read()
-	reg = re.compile(r'\[[^\]]*\]')
-	a = reg.findall(s)
-	for thing in a:
-		d.add(thing)
-	print 'finished ' + i
+        reg = re.compile(r'\[([^\]]*)\]')
+        a = reg.findall(s)
+        for thing in a:
+            d.add(thing)
+    return d
+            
 
-def get_actors(filename):
-    return
-    
 def parse(filename):
     with open(filename) as f:
         content = f.read()
@@ -27,6 +46,6 @@ def parse(filename):
         return L
         
 
-parse('data/alexander-hamilton.txt')
+#parse('data/alexander-hamilton.txt')
 
     
